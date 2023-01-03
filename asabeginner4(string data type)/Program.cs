98951dddc192a1d734eb22a51785273faab4352b
PrabinPanthee <121803999+PrabinPanthee@users.noreply.github.com>
2023-01-03 23:04:59 +0545

    Update Program.cs

--- asabeginner4(string data type)/Program.cs
+++ asabeginner4(string data type)/Program.cs
@@ -5,8 +5,10 @@ class Program
     {
         
         int val;
+        //parse is a data converter 
         val = int.Parse("1235");
         Console.WriteLine("Value is ",val);
+        //equals is a methods that compares two datas gives boolean value 
         flag = int.Equals(1235, 5656);
         if (flag==true) {
             Console.WriteLine("both are equal");
