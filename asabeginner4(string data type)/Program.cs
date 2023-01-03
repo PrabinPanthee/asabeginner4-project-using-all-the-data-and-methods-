143285dc133a1a0238d6c6604b6ef6dfed384a89
iampr <iampr@PRABIN>
2023-01-03 14:42:22 +0545

    Add project files.

---
+++ asabeginner4(string data type)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        
+        int val;
+        val = int.Parse("1235");
+        Console.WriteLine("Value is ",val);
+        flag = int.Equals(1235, 5656);
+        if (flag==true) {
+            Console.WriteLine("both are equal");
+        }
+        else
+        { Console.WriteLine("both are not equal "); }
+    }
+}
+        
+
